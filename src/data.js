ca957674c797dfa802044d61667c38cd92e45ba1
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-10-23 17:32:37 +0200

    add portfolio GIFs and include info in data.js

---
+++ src/data.js
@@ -0,0 +1,25 @@
+export const projects = [
+    {
+        title: "A quick guide to understanding regex",
+        subtitle: "Regex, Markdown, Gist",
+        description: "Use markdown and GitHub Gist to explain the regex for a URL",
+        image: "../public/regex.gif",
+        link: "https://gist.github.com/tlcoles/4e4ef88bb7830296434015e98dfa34e6",
+    },
+    {
+        title: "NoSQL challenge: social network API",
+        subtitle: "MongoDB, NodeJS, Insomnia",
+        description: "Build an API for a social network web application",
+        image: "../public/social-network-api.gif",
+        link: "https://github.com/tlcoles/challenge-18-social-network-api",
+    },
+    {
+        title: "MVC Challenge: Tech Blog",
+        subtitle: "Model-View-Controller, Sequelize, Handelbars, Express",
+        description: "Create a CMS-style tech blog allowing posts and comments",
+        image: "../public/tech-blog.gif",
+        link: "https://github.com/tlcoles/Challenge-14-Tech-Blog",
+    },
+];
+
+// Stretch goal: include skills/technologies
