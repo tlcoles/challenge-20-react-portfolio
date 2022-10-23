4aad4426a2a68198830da4f0ab1d62fe5df6ecbd
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-10-23 09:20:28 +0200

    install tailwindcss, import components in App.js

--- src/App.js
+++ src/App.js
@@ -1,4 +1,10 @@
 import React from "react";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+import Navbar from "./components/Navbar";
+import Project from "./components/Project";
 
 export default function App() {
   return (
