32400f5f0c0df3721cc6c1a1a8a1b5e3775117cb
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-10-26 23:09:08 +0200

    make navbar.js header.js

--- src/App.js
+++ src/App.js
@@ -3,14 +3,13 @@ import About from "./components/About";
 import Contact from "./components/Contact";
 import Footer from "./components/Footer";
 import Header from "./components/Header";
-import Navbar from "./components/Navbar";
+
 import Projects from "./components/Projects";
 
 export default function App() {
   return (
     <main className="text-gray-400 bg-gray-900 body-font">
       <Header />
-      <Navbar />
       <About />
       <Projects />
       <Contact />
