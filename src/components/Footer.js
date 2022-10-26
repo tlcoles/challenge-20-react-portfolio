d3778c21a1e4de6496ed8bc269f5a5242320e6b0
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-10-26 23:22:49 +0200

    add footer.js

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,2 +1,11 @@
 import React from "react";
-export default function Footer() {}
+export default function Footer() {
+    return (
+        <footer>
+          <div className="container mx-auto flex flex-wrap p-5 flex-col md:flex-row items-center">
+            <h4>&copy; {new Date().getFullYear()} - Tammi L. Coles</h4>
+            <p>GitHub <a href="https://github.com/tlcoles">@tlcoles</a> | LinkedIn <a href="https://www.linkedin.com/in/tammilcoles/">tammilcoles</a>/</p>
+          </div>
+        </footer>
+      );
+}
