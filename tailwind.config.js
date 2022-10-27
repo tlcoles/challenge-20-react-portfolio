05eff929b7b0b1018c9415ebfefec0da51be1404
Tammi L. Coles <21356324+tlcoles@users.noreply.github.com>
2022-10-27 05:07:48 +0200

    downgraded tailwind requires purge not content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  content: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
+  purge: ['./src/**/*.{js,jsx,ts,tsx}', './public/index.html'],
   darkMode: 'class',
   theme: {
     extend: {},
